f6ebd5eb55f12bfb055ee710e93c22d9e185e0e7
ennio <dott.rampello@gmail.com>
2022-12-21 13:56:23 +0100

    clean feature pipeline file script to upload file to drive

--- feature-pipeline.py
+++ feature-pipeline.py
@@ -7,22 +7,23 @@ import cv2
 import os
 import argparse
 
+def augment_image(file_path):
+    """
+    Augment the image by rotating it by 10, -10, 30, -30 degrees, flipping it horizontally and increasing/decreasing the brightness
 
-dataset = './dataset/' # path to input dataset directory 
-encodings_file = './encodings.pkl' #path to output encodings file
-detection_method = 'hog'
-
-imagePaths = list(paths.list_images(dataset))
-knownEncodings = []
-knownNames = []
-s = time.time()
-
-for (i, imagePath) in enumerate(imagePaths):
-    name = imagePath.split(os.path.sep)[-2] 
-    print(f"processing image [{name}] {i+1}/{len(imagePaths)}")
+    params:
+        file_path: path to the image file
+    
+    returns:
+        list of 8 augmented images
+    """
+    image = cv2.imread(file_path)
     images = []
-    image = cv2.imread(imagePath)
+    
+    # convert the input image from BGR to RGB
     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # add the original image to the list of images
     images.append(rgb_image)
     # rotate the image by 10 degrees and add it to the list of images
     images.append(imutils.rotate_bound(rgb_image, 10))
@@ -32,38 +33,45 @@ for (i, imagePath) in enumerate(imagePaths):
     images.append(imutils.rotate_bound(rgb_image, 30))
     # rotate the image by -30 degrees and add it to the list of images
     images.append(imutils.rotate_bound(rgb_image, -30))
-
     # flip the image horizontally and add it to the list of images
     images.append(cv2.flip(rgb_image, 1))
-
     #increase the brightness of the image and add it to the list of images
     images.append(cv2.convertScaleAbs(rgb_image, alpha=1.5, beta=0))
     #decrease the brightness of the image and add it to the list of images
     images.append(cv2.convertScaleAbs(rgb_image, alpha=0.5, beta=0))
 
-    print(f'created {len(images)} images for {name}')
+    return images
 
 
-    # detect the (x,y)-coordinates of the bounding boxes
-    # corresponding to each face in the input image
-    # we are assuming the the boxes of faces are the SAME FACE or SAME PERSON
-    # boxes = face_recognition.face_locations(rgb_image, model=detection_method)
-    # print(f"Found {len(boxes)} faces in image")
+if __name__ == "__main__":
 
-    for l in images:
-        boxes = [(0, l.shape[1], l.shape[0], 0)]
-        encodings = face_recognition.face_encodings(l, boxes)
+    dataset = './dataset' # path to input dataset directory
+    encodings_file = './encodings.pkl' #path to output encodings file
 
-        # creating the training set
-        for encoding in encodings:
-            knownEncodings.append(encoding)
-            knownNames.append(name)
+    imagePaths = list(paths.list_images(dataset))
+    known_encodings = []
+    known_names = []
+    s = time.time()
+    print(imagePaths)
+    for i, imagePath in enumerate(imagePaths):
+        images = augment_image(imagePath)
+        name = imagePath.split(os.path.sep)[-2] 
+        print(f"processing image [{name}] {i+1}/{len(imagePaths)}")
+    
+        for l in images:
+            boxes = [(0, l.shape[1], l.shape[0], 0)]
+            encodings = face_recognition.face_encodings(l, boxes)
 
-e = time.time()
-print(f"Encoding dataset took: {(e-s)} seconds")
-data = {"encodings": knownEncodings, "names": knownNames}
-f = open(encodings_file, "wb")
-f.write(pickle.dumps(data))
-f.close()
+            # creating the training set
+            for encoding in encodings:
+                known_encodings.append(encoding)
+                known_names.append(name)
 
-# replace the above few lines with the script to upload this to hopsworks
+    e = time.time()
+    print(f"Encoding dataset took: {(e-s)} seconds")
+    data = {"encodings": known_encodings, "names": known_names}
+    f = open(encodings_file, "wb")
+    f.write(pickle.dumps(data))
+    f.close()
+
+    # replace the above few lines with the script to upload this to hopsworks
